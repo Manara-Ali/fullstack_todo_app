b443ee8840549212d1f12b3384f91426702e6033
Manara-Ali <manaraali64@gmail.com>
2023-04-08 22:48:47 -0400

    Update Modal compoent

--- todoapp/client/src/components/Modal.jsx
+++ todoapp/client/src/components/Modal.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createPortal } from "react-dom";
+import { ModalContext } from "../context/ModalContext";
 
 const Modal = () => {
+  const { modalOpen, setModalOpen } = useContext(ModalContext);
+
+  const handleModalClose = () => {
+    document.querySelector(".app-container").classList.remove("blur");
+    setModalOpen(false);
+  };
   return createPortal(
-    <div className="modal-container">
-      <div className="modal-content">
-        <h1>I am a modal</h1>
-      </div>
+    <div
+      className="modal-container"
+      onClick={() => {
+        return handleModalClose();
+      }}
+    >
+      <div className="modal-content">{/* <h1>I am a modal</h1> */}</div>
     </div>,
     document.querySelector("#portal-container")
   );
