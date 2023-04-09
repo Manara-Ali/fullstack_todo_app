e7554fef3943b3aede0c1f4d30a0c7c5db4b420f
Manara-Ali <manaraali64@gmail.com>
2023-04-08 20:31:11 -0400

    Create a todo form

--- todoapp/client/src/components/TodoForm.jsx
+++ todoapp/client/src/components/TodoForm.jsx
@@ -1,14 +1,47 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { enterTerm, createTodo } from "../store";
 
 const TodoForm = () => {
+  const dispatch = useDispatch();
+  const { formTerm } = useSelector((state) => {
+    return state.formCombinedReducer;
+  });
+
+  // Create a function to create new todo tasks
+  const handleCreateTodo = (todo) => {
+    const todoTask = {
+      id: nanoid(),
+      task: todo,
+      completed: false,
+      createdAt: Date.now(),
+    };
+    dispatch(createTodo(todoTask));
+  };
+
   return (
     <div className="ui segment container">
       <div className="ui form">
         <div className="field">
           <label className="todo-label">Todo Task</label>
-          <input type="text" name="todo" placeholder="Enter Todo Item" />
+          <input
+            type="text"
+            name="todo"
+            placeholder="Enter Todo Item"
+            onChange={(e) => {
+              return dispatch(enterTerm(e.target.value));
+            }}
+            value={formTerm}
+          />
         </div>
-        <button className="ui button" type="submit">
+        <button
+          className="ui button"
+          type="submit"
+          onClick={() => {
+            return handleCreateTodo(formTerm);
+          }}
+        >
           Submit
         </button>
       </div>
