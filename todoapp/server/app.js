3e00ca32112c6d843a4bf014ec0f18fbdcf65d9a
Manara-Ali <manaraali64@gmail.com>
2023-04-09 17:31:14 -0400

    Added cors

--- todoapp/server/app.js
+++ todoapp/server/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const todoRouter = require("./routes/todoRoutes");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
