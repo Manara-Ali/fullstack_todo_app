bfe4f6367fa67ddf59afa4ffcea4c04ccb69f355
Manara-Ali <manaraali64@gmail.com>
2023-04-08 22:33:55 -0400

    Import custom provider to index.js

--- todoapp/client/src/index.js
+++ todoapp/client/src/index.js
@@ -3,10 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { CustomProvider } from "./context/ModalContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <Provider store={store}>
-    <App />
+    <CustomProvider>
+      <App />
+    </CustomProvider>
   </Provider>
 );
