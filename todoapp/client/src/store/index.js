3e5a67a129864474f89eb95a38c05fbae52ead26
Manara-Ali <manaraali64@gmail.com>
2023-04-08 20:33:00 -0400

    Import createTodo function

--- todoapp/client/src/store/index.js
+++ todoapp/client/src/store/index.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { todosCombinedReducer } from "./slices/todosSlice";
+import { formCombinedReducer, enterTerm } from "./slices/formSlice";
 import { fetchTodos } from "./thunks/fetchTodos";
+import { createTodo } from "./thunks/createTodo";
 
 export const store = configureStore({
   reducer: {
     todosCombinedReducer,
+    formCombinedReducer,
   },
 });
 
-export { fetchTodos };
+export { fetchTodos, createTodo, enterTerm };
